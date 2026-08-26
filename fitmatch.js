7f0c487f9a2944cad1f137d10dd081e711d49d96
Olexsander07-source <Olexsander07@icloud.com>
2026-08-26 08:22:49 +0200

    Update fitmatch.js

--- fitmatch.js
+++ fitmatch.js
@@ -4,8 +4,8 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // ВСТАВЬ СЮДА ДАННЫЕ ИЗ SUPABASE
 // ==========================================
 
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = https://ypbhcgcwkpiujcakvaji.supabase.co;
+const SUPABASE_ANON_KEY = sb_publishable_Lsrk07A5aXJH7YypVR8QGQ_TQPwhfOV;
 
 // ==========================================
 
